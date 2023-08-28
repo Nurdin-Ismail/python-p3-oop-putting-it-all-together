a5f0ff634e4416c8d2fa9a238f8cf6fa631e8b6e
Nurdin Ismail <nurdin.ismail@student.moringaschool.com>
2023-08-28 22:52:37 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    
+     def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        
+     def get_page_count(self):
+        return self._page_count
+    
+     def validate_page_count(self, page_count):
+        if(type(page_count) == int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+     page_count = property(get_page_count, validate_page_count)
+    
+     def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+fido = Book('kanu', 92)
+
+
+print(fido.page_count)
+
+
+
+
     
         
